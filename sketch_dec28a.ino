b1a0e71b0bd6d447cd08c7b2bfdb02a8a1169c0e
U Peravich <85332850+pperavichhh@users.noreply.github.com>
2023-01-06 16:49:42 +0700

    Update code
    still can't display a pm2.5 problem at a pms3003 and electrical circuit of that

--- sketch_dec28a.ino
+++ sketch_dec28a.ino
@@ -1,59 +1,44 @@
 #include <DHT.h> // include the DHT library
 #include <SoftwareSerial.h> // include the SoftwareSerial library
 
-#define DHTTYPE DHT11 // specify the type of DHT sensor you are using
-#define DHTPIN 4 // specify the pin the DHT is connected to
+#define DHTPIN 2 // define the pin the DHT11 is connected to
+#define DHTTYPE DHT11 // define the DHT11 sensor type
+DHT dht(DHTPIN, DHTTYPE); // create an instance of the DHT class
 
-DHT dht(DHTPIN, DHTTYPE); // create a DHT object
-
-#define RX_PIN 2 // specify the RX pin for the PMS3003 sensor
-#define TX_PIN 3 // specify the TX pin for the PMS3003 sensor
-
-SoftwareSerial pmSerial(RX_PIN, TX_PIN); // create a SoftwareSerial object for the PMS3003 sensor
+#define RX_PIN 10 // define the RX pin for the PMS3003
+#define TX_PIN 11 // define the TX pin for the PMS3003
+SoftwareSerial pmsSerial(RX_PIN, TX_PIN); // create an instance of the SoftwareSerial class
 
 void setup() {
-  // initialize the serial monitor
-  Serial.begin(9600);
-  // initialize the DHT sensor
-  dht.begin();
-  // initialize the SoftwareSerial object for the PMS3003 sensor
-  pmSerial.begin(9600);
+  Serial.begin(9600); // start the serial monitor
+  dht.begin(); // start the DHT11 sensor
+  pmsSerial.begin(9600); // start the PMS3003 sensor
 }
 
 void loop() {
-  // read the humidity and temperature from the DHT sensor
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  // read data from the DHT11 sensor
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  // check if the DHT read was successful
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-  } else {
-    // print the humidity and temperature to the serial monitor
-    Serial.print("Humidity: ");
-    Serial.print(h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(t);
-    Serial.println(" *C ");
-  }
+  // print the humidity and temperature to the serial monitor
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.print("%\tTemperature: ");
+  Serial.print(temperature);
+  Serial.println("°C");
 
   // read data from the PMS3003 sensor
-  if (pmSerial.available()) {
-    // create a buffer to store the incoming data
-    byte pmData[32];
-    // read the data from the sensor into the buffer
-    pmSerial.readBytes(pmData, 32);
-    // check if the data is valid by checking the first byte
-    if (pmData[0] == 0x42) {
-      // calculate the PM2.5 concentration in ug/m^3
-      int pm25 = (pmData[13] << 8) + pmData[14];
-      // print the PM2.5 concentration to the serial monitor
-      Serial.print("PM2.5: ");
-      Serial.print(pm25);
-      Serial.println(" ug/m^3");
-    }
+  if (pmsSerial.available()) {
+    byte buffer[24];
+    pmsSerial.readBytes(buffer, 24);
+    int pm25 = (buffer[16] << 8 | buffer[17]); // get the PM2.5 value
+    int pm10 = (buffer[18] << 8 | buffer[19]); // get the PM10 value
+
+    // print the PM2.5 value to the serial monitor
+    Serial.print("PM2.5: ");
+    Serial.print(pm25);
+    Serial.println(" μg/m^3");
   }
-  // wait 1 second before reading the sensors again
-  delay(1000);
+
+  delay(1000); // wait 1 second before reading again
 }
