60b895c1d27e7d6557a0551c541542239db5d365
U Peravich <85332850+pperavichhh@users.noreply.github.com>
2023-01-17 16:09:49 +0700

    Update sketch_dec28a.ino

--- sketch_dec28a.ino
+++ sketch_dec28a.ino
@@ -1,44 +1,55 @@
-#include <DHT.h> // include the DHT library
-#include <SoftwareSerial.h> // include the SoftwareSerial library
-
-#define DHTPIN 2 // define the pin the DHT11 is connected to
-#define DHTTYPE DHT11 // define the DHT11 sensor type
-DHT dht(DHTPIN, DHTTYPE); // create an instance of the DHT class
-
-#define RX_PIN 10 // define the RX pin for the PMS3003
-#define TX_PIN 11 // define the TX pin for the PMS3003
-SoftwareSerial pmsSerial(RX_PIN, TX_PIN); // create an instance of the SoftwareSerial class
-
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2,3); // RX, TX
+unsigned int pm1 = 0;
+unsigned int pm2_5 = 0;
+unsigned int pm10 = 0;
 void setup() {
-  Serial.begin(9600); // start the serial monitor
-  dht.begin(); // start the DHT11 sensor
-  pmsSerial.begin(9600); // start the PMS3003 sensor
+Serial.begin(9600);
+while (!Serial) ;
+mySerial.begin(9600);
 }
-
+ 
 void loop() {
-  // read data from the DHT11 sensor
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  // print the humidity and temperature to the serial monitor
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print("%\tTemperature: ");
-  Serial.print(temperature);
-  Serial.println("°C");
-
-  // read data from the PMS3003 sensor
-  if (pmsSerial.available()) {
-    byte buffer[24];
-    pmsSerial.readBytes(buffer, 24);
-    int pm25 = (buffer[16] << 8 | buffer[17]); // get the PM2.5 value
-    int pm10 = (buffer[18] << 8 | buffer[19]); // get the PM10 value
-
-    // print the PM2.5 value to the serial monitor
-    Serial.print("PM2.5: ");
-    Serial.print(pm25);
-    Serial.println(" μg/m^3");
-  }
-
-  delay(1000); // wait 1 second before reading again
+int index = 0;
+char value;
+char previousValue;
+ 
+while (mySerial.available()) {
+value = mySerial.read();
+if ((index == 0 && value != 0x42) || (index == 1 && value != 0x4d)){
+Serial.println("Cannot find the data header.");
+break;
+}
+ 
+if (index == 4 || index == 6 || index == 8 || index == 10 || index == 12 || index == 14) {
+previousValue = value;
+}
+else if (index == 5) {
+pm1 = 256 * previousValue + value;
+Serial.print("{ ");
+Serial.print("\"pm1\": ");
+Serial.print(pm1);
+Serial.print(" ug/m3");
+Serial.print(", ");
+}
+else if (index == 7) {
+pm2_5 = 256 * previousValue + value;
+Serial.print("\"pm2_5\": ");
+Serial.print(pm2_5);
+Serial.print(" ug/m3");
+Serial.print(", ");
+}
+else if (index == 9) {
+pm10 = 256 * previousValue + value;
+Serial.print("\"pm10\": ");
+Serial.print(pm10);
+Serial.print(" ug/m3");
+} else if (index > 15) {
+break;
+}
+index++;
+}
+while(mySerial.available()) mySerial.read();
+Serial.println(" }");
+delay(1000);
 }
